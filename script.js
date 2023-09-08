75ef5dc00f8005f07a614640510328ed11204d74
rotradam <91180828+rotradam@users.noreply.github.com>
2023-09-08 11:43:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ submitButton.addEventListener('click', function(event) {
     // Redirect the user to the MoonPay URL
     window.location.href = moonpayUrl;
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const moonpaySdk = window.MoonPayWebSdk.init({  
+        flow: 'buy',
+        environment: 'production',
+        variant: 'overlay',  
+        params: {  
+            apiKey: 'pk_live_ggMIn2nJaeNslwe4m9NiEGz038xl1V'
+        }
+    });
+
+    document.querySelector("#continueBtn").addEventListener("click", function(e) {
+        e.preventDefault(); // Prevent any default behavior
+        moonpaySdk.show();
+    });
+});
